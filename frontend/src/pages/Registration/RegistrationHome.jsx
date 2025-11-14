5ec7e13ebd9392a7fba5b9a0fa4bbbf255554de6
PratyutCS <pratyut987@gmail.com>
2025-11-14 21:16:04 +0530

    yet you still did it

--- frontend/src/pages/Registration/RegistrationHome.jsx
+++ frontend/src/pages/Registration/RegistrationHome.jsx
@@ -33,7 +33,7 @@ export default function RegistrationHome() {
               </p>
 
               <div>
-                <h3 className="text-2xl font-bold text-[#2e2a30] mb-4">Registration Benefits</h3>
+                <h3 className="text-2xl font-bold text-[#2e2a30] mb-4">Participation Benefits</h3>
                 <ul className="space-y-4 text-[#2e2a30]/80">
                   <li className="flex items-start">
                     <svg className="w-6 h-6 text-[#7c3aed] mr-3 mt-1 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h10a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z" /></svg>
